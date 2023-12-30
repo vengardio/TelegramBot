c8a77ea1f68c281ce2cd2f4eaad5541d1af11c00
vengardio <101064067+vengardio@users.noreply.github.com>
2023-12-30 18:19:32 +0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,37 +1,45 @@
 import telebot
 import config
 import time 
+from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 
-def to_logs(message): 
+def to_logs_message(message): 
     with open("log.txt", "a", encoding='utf-8') as logs: #write user message to logs
         newLine = "\n" + time.ctime(time.time()) + " - "  +  str(message.from_user.first_name) + ": " + str(message.text)
+        logs.write(newLine)
+        
+def to_logs_text(message, text): 
+    with open("log.txt", "a", encoding='utf-8') as logs: #write user message to logs
+        newLine = "\n" + time.ctime(time.time()) + " - "  +  str(message.from_user.first_name) + ": " + str(text)
         logs.write(newLine)
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    to_logs(message)
+    to_logs_message(message)
     bot.send_message(message.chat.id, f'<B>Привет, {message.from_user.first_name}, я Борис, тупое сотворение информатики!</B>', parse_mode="html")
 
-@bot.message_handler(commands=['id'])
-def user_id(message):
-    to_logs(message)
-    bot.send_message(message.chat.id, f"Your ID:{message.from_user.id}")
+@bot.message_handler(commands=['owner'])
+def owners_info(message):
+    to_logs_message(message)
+
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton("Телеграмм", url="https://t.me/StpdCow")
+    btn2 = types.InlineKeyboardButton("GitHub", url="https://github.com/vengardio")
+    markup.row(btn1, btn2)
+    markup.add(types.InlineKeyboardButton("А я просто кнопка)", callback_data = "button_answer"))
     
-@bot.message_handler(commands=['git'])
-def user_id(message):
-    to_logs(message)
-    bot.send_message(message.chat.id, "Owner's Git page: https://github.com/vengardio")    
+    bot.send_message(message.chat.id, f'Вот контактные данные моего создателя', reply_markup=markup)
+    
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == "button_answer":
+        to_logs_text(callback, "/нажал на тупую кнопку/")
+        bot.send_message(callback.message.chat.id, "ну и что, ну нажал ты, что дальше то?")
 
 @bot.message_handler(content_types=['text'])
-def retype(message):
-    to_logs(message)
-    bot.send_message(message.chat.id, message.text)
-    
-@bot.message_handler(content_types=['sticker'])
-def retype(message):
-    to_logs(message)
-    bot.send_message(message.chat.id, "Я не понимаю тебя!!! ")
+def default(message):
+    to_logs_message(message)
     
 bot.polling(none_stop=True) 
