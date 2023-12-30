f2348e86cfa359a9d6185a8076c9825636734a5f
vengardio <101064067+vengardio@users.noreply.github.com>
2023-12-30 13:58:08 +0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 import telebot
 import config
+import time 
 
 bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
+    with open("log.txt", "a", encoding='utf-8') as logs:
+        newLine = "\n" + time.ctime(time.time()) + " - "  +  str(message.from_user.first_name) + ": " + str(message.text)
+        logs.write(newLine)
+    #bot.send_message(message.chat.id, "Извините, мадам, я не могу вам писать, вы занятая дама...")
     bot.send_message(message.chat.id, message.text)
     
 bot.polling(none_stop=True) 
